8722fb9747e7db5e36b590ef6b3d112268a6ec51
Ryan Sullivan <ryan.sullivan0319@gmail.com>
2021-10-05 20:16:22 -0600

    Create a function to randomly select a choice from each of the arrays

--- dtg.js
+++ dtg.js
@@ -11,3 +11,17 @@ let entertainmentArray = ["Live Music", 'Midget Wrestling', 'Poetry Slam', 'Foot
 // console.log(restaurantArray);
 // console.log(transportationArray);
 // console.log(entertainmentArray);
+
+
+// // Function to randomly select a choice from each array 
+
+function selectRandom(choiceArray){
+    return choiceArray[Math.floor(Math.random()*choiceArray.length)];
+
+}
+
+let destination = selectRandom(destinationArray);
+let restaurant = selectRandom(restaurantArray);
+let transport = selectRandom(transportationArray);
+let entertain = selectRandom(entertainmentArray);
+// console.log(destination, restaurant, transport, entertain);
