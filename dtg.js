42d2e5b0af2529115111edaa9156d520d8a54806
Ryan Sullivan <ryan.sullivan0319@gmail.com>
2021-10-05 19:53:53 -0600

    create arrays to hold data for random selection (destination, restaurant, transportation, entertainment)

--- dtg.js
+++ dtg.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+// Day Trip Generator
+
+// create arrays with data for (destination, restaurant, transport and entertainment)
+
+let destinationArray = ['Egypt', 'Mars', 'Fiji', 'Graceland'];
+let restaurantArray = ['American', 'Chinese', 'Italian', 'Indian'];
+let transportationArray = ['Plane', 'Train', 'Automobile', 'Rickshaw'];
+let entertainmentArray = ["Live Music", 'Midget Wrestling', 'Poetry Slam', 'Football Game'];
+
+// console.log(destinationArray);
+// console.log(restaurantArray);
+// console.log(transportationArray);
+// console.log(entertainmentArray);
