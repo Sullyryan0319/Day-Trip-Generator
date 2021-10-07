e49957c3da460a11ef2dee8071d075cc3f124e79
Ryan Sullivan <ryan.sullivan0319@gmail.com>
2021-10-06 20:32:00 -0600

    clean up if/else statments so they re-define array indexes.

--- dtg.js
+++ dtg.js
@@ -1,8 +1,8 @@
-// Day Trip Generator
+// // Day Trip Generator
 
 
 
-// create arrays with data for (destination, restaurant, transport and entertainment)
+// // create arrays with data for (destination, restaurant, transport and entertainment)
 
 let destinationArray = ['Egypt', 'Mars', 'Fiji', 'Graceland'];
 let restaurantArray = ['American', 'Chinese', 'Italian', 'Indian'];
@@ -29,6 +29,8 @@ console.log(destination, restaurant, transport, entertain);
 let itineraryArray = [destination, restaurant, transport, entertain];
 
 let isUserHappy = false;
+let presentTrip;
+
 
 while (isUserHappy === false) {
 
@@ -36,37 +38,37 @@ while (isUserHappy === false) {
         ", your transportation will be " + itineraryArray[2] + ", and your entertainment will be " + itineraryArray[3];
     alert(presentTrip);
 
-    userInput = prompt("Please select one of the options:"
+    let userInput = prompt("Please select one of the options:"
         + "1 for new location "
         + ", 2 for new restaurant "
         + ", 3 for new transportation "
         + ", 4 for new entertainment "
         + ", 5 to start fresh "
         + ", 6 to confirm selections");
-        if (userInput === "1") {
-            destination = selectRandom(destinationArray);
-        } else if (userInput === "2") {
-            restaurant = selectRandom(restaurantArray);
-        } else if (userInput === "3") {
-            transport = selectRandom(transportationArray);
-        } else if (userInput === "4") {
-            entertain = selectRandom(entertainmentArray);
-        } else if (userInput = "5") {
-            destination = selectRandom(destinationArray);
-            restaurant = selectRandom(restaurantArray);
-            transport = selectRandom(transportationArray);
-            entertain = selectRandom(entertainmentArray);
-        } else if (userInput === "6") {
-            isUserHappy === true;
-            alert("Trip Confirmed! " + presentTrip);
-            break;
-    
-        } else {
-            alert("Please enter a valid number.");
-    
-        }
-    
-    
+    if (userInput === "1") {
+        itineraryArray[0] = selectRandom(destinationArray);
+    } else if (userInput === "2") {
+        itineraryArray[1] = selectRandom(restaurantArray);
+    } else if (userInput === "3") {
+        itineraryArray[2] = selectRandom(transportationArray);
+    } else if (userInput === "4") {
+        itineraryArray[3] = selectRandom(entertainmentArray);
+    } else if (userInput === "5") {
+        itineraryArray[0] = selectRandom(destinationArray);
+        itineraryArray[1] = selectRandom(restaurantArray);
+        itineraryArray[2] = selectRandom(transportationArray);
+        itineraryArray[3] = selectRandom(entertainmentArray);
+    } else if (userInput === "6") {
+        isUserHappy = true;
+        alert("Trip Confirmed! " + presentTrip);
+        break;
+
+    } else {
+        alert("Please enter a valid number.");
+
     }
-    console.log(presentTrip);
+
+
+}
+console.log(presentTrip);
 
