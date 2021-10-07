284500e5ee877de25937e48573f4ecf36ecaf862
Ryan Sullivan <ryan.sullivan0319@gmail.com>
2021-10-06 19:24:05 -0600

    Create a while loop to present the itenerary. Prompt the user to change selections they aren't happy with.

--- dtg.js
+++ dtg.js
@@ -1,5 +1,7 @@
 // Day Trip Generator
 
+
+
 // create arrays with data for (destination, restaurant, transport and entertainment)
 
 let destinationArray = ['Egypt', 'Mars', 'Fiji', 'Graceland'];
@@ -7,16 +9,11 @@ let restaurantArray = ['American', 'Chinese', 'Italian', 'Indian'];
 let transportationArray = ['Plane', 'Train', 'Automobile', 'Rickshaw'];
 let entertainmentArray = ["Live Music", 'Midget Wrestling', 'Poetry Slam', 'Football Game'];
 
-// console.log(destinationArray);
-// console.log(restaurantArray);
-// console.log(transportationArray);
-// console.log(entertainmentArray);
-
 
 // // Function to randomly select a choice from each array 
 
-function selectRandom(choiceArray){
-    return choiceArray[Math.floor(Math.random()*choiceArray.length)];
+function selectRandom(choiceArray) {
+    return choiceArray[Math.floor(Math.random() * choiceArray.length)];
 
 }
 
@@ -24,4 +21,26 @@ let destination = selectRandom(destinationArray);
 let restaurant = selectRandom(restaurantArray);
 let transport = selectRandom(transportationArray);
 let entertain = selectRandom(entertainmentArray);
-// console.log(destination, restaurant, transport, entertain);
+console.log(destination, restaurant, transport, entertain);
+
+
+// // function that allows user to re-select any or all of the random choices
+
+let itineraryArray = [destination, restaurant, transport, entertain];
+
+let isUserHappy = false;
+
+while (isUserHappy === false) {
+
+    presentTrip = "Your destination will be " + itineraryArray[0] + ", your restaurant will be " + itineraryArray[1] +
+        ", your transportation will be " + itineraryArray[2] + ", and your entertainment will be " + itineraryArray[3];
+    alert(presentTrip);
+
+    userInput = prompt("Please select one of the options:"
+        + "1 for new location "
+        + ", 2 for new restaurant "
+        + ", 3 for new transportation "
+        + ", 4 for new entertainment "
+        + ", 5 to start fresh "
+        + ", 6 to confirm selections");
+}
